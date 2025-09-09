596d7b18f162ab81d5ebffe5e7e99570b820a628
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-09-10 00:27:13 +0700

    feat: enhance Next.js configuration with experimental CSS optimization and update output tracing includes for OpenGraph templates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingIncludes: {
-    "/*": "og-template/**/*",
+    "/*": ["og-template/**/*"],
+  },
+  experimental: {
+    optimizeCss: true,
   },
 }
 
